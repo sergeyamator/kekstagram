65e9f2b1505c894b82f992ce21d41f209edf4188
Sergey Rubets <sramator@gmail.com>
2016-05-25 13:52:39 +0300

    Start programming.

---
+++ build/js/check.js
@@ -0,0 +1,25 @@
+'use strict';
+
+function getMessage(a, b) {
+  if (typeof a === 'boolean') {
+    if (a) {
+      return 'Переданное GIF-изображение анимировано и содержит ' + b + ' кадров';
+    } else {
+      return 'Переданное GIF-изображение не анимировано';
+    }
+  } else if (typeof a === 'number') {
+    return 'Переданное SVG-изображение содержит ' + a + ' объектов и ' + (b * 4) + ' атрибутов';
+  } else if ((a instanceof Array) && (b instanceof Array)) {
+    var square = a.reduce(function(res, current, index) {
+      return res + (curr * b[index]);
+    });
+
+    return 'Общая площадь артефактов сжатия: ' + square +  'пикселей';
+  } else if (a instanceof Array) {
+    var sum = a.reduce(function(sum, current) {
+      return sum + current;
+    });
+
+    return 'Количество красных точек во всех строчках изображения: ' + sum;
+  }
+}
