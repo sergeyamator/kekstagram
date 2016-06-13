824b35f09172700e812c5d9c8b671ac144576797
Sergey Rubets <sergeyglyharev@gmail.com>
2016-06-13 08:59:13 +0300

    Filters by new, popular and discussed were added.

--- src/pictures.js
+++ src/pictures.js
@@ -53,19 +53,30 @@ if (supportsTemplate()) {
  * @param {function(Array.<Object>)} callback
  */
 function getPictures(callback) {
-  var xhr = new XMLHttpRequest();
+  var xhr = new XMLHttpRequest(),
+    pictures = document.querySelector('.pictures');
 
   xhr.addEventListener('load', function(evt) {
     var loadedData = JSON.parse(evt.target.response);
     callback(loadedData);
+
+    pictures.classList.remove('pictures-loading');
+    filters.classList.remove('hidden');
   });
 
   xhr.open('GET', PICTURES_LOAD_URL);
   xhr.send();
+
+  pictures.classList.add('pictures-loading');
+
+  xhr.addEventListener('error', function() {
+    pictures.classList.add('pictures-failure');
+  });
 }
 
 getPictures(function(loadedPictures) {
   window.pictures = loadedPictures;
+  setFiltraionEnabled();
   renderPictures(window.pictures);
 });
 
@@ -74,7 +85,48 @@ getPictures(function(loadedPictures) {
  * @param {Array.<Object>} pictures
  */
 function renderPictures(pictures) {
+  pictureContainer.innerHTML = '';
+
   pictures.forEach(function(picture) {
     getPictureElement(picture, pictureContainer);
   });
+}
+
+function setFiltraionEnabled() {
+  var filtersForm = document.querySelector('.filters');
+
+  filtersForm.addEventListener('change', function(evt) {
+    var currentElement = evt.target;
+    if (currentElement.tagName === 'INPUT') {
+      setFilterEnabled(currentElement.id);
+    }
+  });
+}
+
+
+/**
+ *
+ * @param {string} filter
+ */
+function setFilterEnabled(filter) {
+  var filteredPictures = getFilteredPictures(window.pictures, filter);
+  renderPictures(filteredPictures);
+}
+
+function getFilteredPictures(pictures, filter) {
+  var picturesToFilter = pictures.slice(0);
+
+  if (filter === 'filter-new') {
+    picturesToFilter.sort(function(a, b) {
+      return new Date(b.date).getTime() - new Date(a.date).getTime();
+    });
+  }
+
+  if (filter === 'filter-discussed') {
+    picturesToFilter.sort(function(a, b) {
+      return b.comments - a.comments;
+    });
+  }
+
+  return picturesToFilter;
 }
